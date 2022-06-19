140324bd7c9d9429d6e236ced8887ab9646ab271
Adam Gibbons <adam.p.gibbons@gmail.com>
2022-06-19 14:49:57 +0100

    Show movie review excerpts; refactor details page

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -6,10 +6,15 @@ import MonetizationIcon from "@material-ui/icons/MonetizationOn";
 import StarRate from "@material-ui/icons/StarRate";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+// New
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Fab from "@material-ui/core/Fab";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from "../movieReviews";
 
 const useStyles = makeStyles((theme) => ({
   chipRoot: {
-    // display: "flex",
+    display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
@@ -30,11 +35,17 @@ const useStyles = makeStyles((theme) => ({
   chipLabel: {
     margin: theme.spacing(0.5),
   },
+  fab: {
+    //New
+    position: "fixed",
+    top: theme.spacing(15),
+    right: theme.spacing(2),
+  },
 }));
 
-const MovieDetails = (props) => {
+const MovieDetails = ({ movie }) => {
   const classes = useStyles();
-  const movie = props.movie;
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
   return (
     <>
@@ -68,21 +79,28 @@ const MovieDetails = (props) => {
           />
           <Chip
             icon={<StarRate />}
-            label={`${movie.vote_average} (${movie.vote_count})`}
+            label={`${movie.vote_average} (${movie.vote_count}`}
           />
           <Chip label={`Released: ${movie.release_date}`} />
         </Paper>
-        <Paper component="ul" className={classes.chipSet}>
-          <Chip
-            label="Production Countries"
-            className={classes.chipLabel}
-            color="primary"
-          />
-          {movie.production_countries.map((c) => (
-            <Chip label={c.name} />
-          ))}
-        </Paper>
       </div>
+      {/* New */}
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <MovieReviews movie={movie} />
+      </Drawer>
     </>
   );
 };
